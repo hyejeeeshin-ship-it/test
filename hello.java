a088f9f0ed6e1e3a4b750ebb3d1dbc6f4d7a76a8
hyejeeeshin-ship-it <hyejeee.shin@gmail.com>
2025-09-15 16:14:40 +0900

    Update hello.java
    Create new file

--- hello.java
+++ hello.java
@@ -2,10 +2,9 @@ package com.salesforce.basics;
 
 public class Hello {
 
-public static void main (String[] args) {
+  public static void main (String[] args) {
 
-  System.out.printIn("Hello, Java");
+    System.out.printIn("Hello, Java");
 
-}
-
+  }
 }
